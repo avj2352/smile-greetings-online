5c4d45d2f48e80a04fb64455d2b44e16ec7168e3
Pramod Jingade <avj2352@gmail.com>
2017-12-26 20:51:32 +0530

    Initial setup of the project

--- src/views/login/login.view.jsx
+++ src/views/login/login.view.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './../../assets/logo/logo.png';
+import classnames from 'classnames';
 import './login.css';
 
 class Login extends Component {
     render() {
         return (
             <div className="login">
-                OAuth Login
+                <header>
+                    <div className="logo-image">
+                        <img src={logo} width="10%" height="auto"/>
+                    </div>
+                    <div className="title">Smile Greetings Online</div>
+                </header>
             </div>
         );
     }//end:render
