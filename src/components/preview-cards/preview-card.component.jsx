0c396f7fb7c0eca2791f1c6142ec861eeba0764e
Pramod Jingade <avj2352@gmail.com>
2017-12-27 11:15:11 +0530

    Created login and card preview sections

---
+++ src/components/preview-cards/preview-card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreviewCard = (props,context)=>{
+    return (
+        <div>
+            Preview Card comes here !!
+        </div>
+    );
+}
+
+export default PreviewCard;
--- src/views/login/login.view.jsx
+++ src/views/login/login.view.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PreviewCard from './../../components/preview-cards/preview-card.component';
+//CSS and Assets
 import logo from './../../assets/logo/logo.png';
 import classnames from 'classnames';
 import './login.css';
@@ -9,11 +11,19 @@ class Login extends Component {
             <div className="login">
                 <header>
                     <div className="logo-image">
-                        <img src={logo} width="10%" height="auto"/>
+                        <img src={logo} width="80%" height="auto"/>
                     </div>
-                    <div className="title">Smile Greetings Online</div>
-                    <div className="subtitle">Create.Share.Celebrate</div>
+                    <div className="logo-content">
+                        <div className="title">Smile Greetings Online</div>
+                        <div className="subtitle">Create.Share.Celebrate</div>
+                    </div>
                 </header>
+                <section>
+                    <div className="login-modal-section"></div>
+                    <div className="rotating-card-section">
+                        <PreviewCard></PreviewCard>
+                    </div>
+                </section>
             </div>
         );
     }//end:render
