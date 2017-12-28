181683c03423080bf36c7418e268af88354d70b1
Pramod Jingade <avj2352@gmail.com>
2017-12-28 19:17:45 +0530

    Integrated firebase plugin into application

--- src/views/login/login.view.jsx
+++ src/views/login/login.view.jsx
@@ -3,9 +3,35 @@ import PreviewCard from './../../components/preview-cards/preview-card.component
 //CSS and Assets
 import logo from './../../assets/logo/logo.png';
 import classnames from 'classnames';
+import { logout } from './../../helpers/authentication/auth';
+import { firebaseAuth } from './../../config/firebaseConfig';
 import './login.css';
 
 class Login extends Component {
+    constructor(props,context){
+        super(props);
+        this.state = { messages: []};
+    }//end:constructor
+
+    componentWillMount(){
+        this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log('User is authenticated !!', user);
+                this.setState({
+                    authed: true,
+                    loading: false,
+              })
+            } else {
+                console.log('User is NOT authenticated: ', user);
+                this.setState({
+                    authed: false,
+                    loading: false
+                })
+            }
+          });
+
+    }//end:componentWillMount
+
     render() {
         return (
             <div className="login">
