21a74be7148b7974e8499e1fb6c1c9cf7c0dddb7
avj2352 <pramod.jingade@philips.com>
2018-01-03 14:53:10 +0530

    Separated Rotation CSS from Card styling CSS

--- src/views/demo-card/demo-card.view.jsx
+++ src/views/demo-card/demo-card.view.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './demo-card.css';
+import './demo-card-rotation.css';
 
 class DemoCard extends Component {
     render() {
@@ -11,7 +12,16 @@ class DemoCard extends Component {
                     <div className="card">
                     {/* Front side of the card */}
                     <div className="card__side card__side--front">
-                        FRONT SIDE OF THE CARD
+                        <div className="top-section">
+                            <h1>Happy Vacations</h1>
+                        </div>
+                        <div className="middle-section">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum sequi sed? Ipsa quo, et tempora vel hic accusantium, nesciunt totam voluptatem dolore sequi vitae quas consectetur aspernatur, animi odit!</p>
+                        </div>
+                        <div className="bottom-section">
+                            <p>From Pramod &amp; Shwetha</p>
+                            <p><a href="#">turnAround</a></p>                            
+                        </div>
                     </div>
                     {/* Back side of the card */}
                     <div className="card__side card__side--back card__side--back-1">
