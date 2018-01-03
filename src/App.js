95d7a38529ad7fb92f32b0f40b5f6a9d57a204e3
avj2352 <pramod.jingade@philips.com>
2018-01-03 20:20:22 +0530

    Changed Routing from Browser to Hash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { HashRouter as Router, Route} from 'react-router-dom';
 import Login from './views/login/login.view';
 import DemoCard from './views/demo-card/demo-card.view';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import './index.css';
 
 class App extends Component {
-  render() {
+  render() {  
     return (
       <Router>
-        <div>          
-          <Route exact path="/" component={Login}/>          
-          <Route exact path="/demo" component={DemoCard}/>          
+        <div>
+          <Route path="/" component={Login}/>
+          <Route path="/demo" component={DemoCard}/>
         </div>
       </Router>
     );
-  }
-}
+  }//end:render
+}//end:App
 
 export default App;
