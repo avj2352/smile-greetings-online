8a87d5d0d1192485bfb40a7e457bd902db0bec1f
avj2352 <pramod.jingade@philips.com>
2018-01-03 19:45:52 +0530

    Animated background

--- src/views/demo-card/demo-card.view.jsx
+++ src/views/demo-card/demo-card.view.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './demo-card.css';
 import './demo-card-rotation.css';
+import couplePhoto from '../../assets/img/lovely-couple.PNG';
 
 class DemoCard extends Component {
     render() {
@@ -15,12 +16,14 @@ class DemoCard extends Component {
                         <div className="top-section">
                             <h1>Happy Vacations</h1>
                         </div>
+                        <div className="photo-section">
+                            <img src={couplePhoto} alt="couple-photo"/>
+                        </div>
                         <div className="middle-section">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum sequi sed? Ipsa quo, et tempora vel hic accusantium, nesciunt totam voluptatem dolore sequi vitae quas consectetur aspernatur, animi odit!</p>
+                            <p>" Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum sequi sed? Ipsa quo, et tempora vel hic accusantium, nesciunt totam voluptatem dolore sequi vitae quas consectetur aspernatur, animi odit! "</p>                            
                         </div>
                         <div className="bottom-section">
-                            <p>From Pramod &amp; Shwetha</p>
-                            <p><a href="#">turnAround</a></p>                            
+                            <p>From Pramod &amp; Shwetha <a href="#">turnAround</a></p>                            
                         </div>
                     </div>
                     {/* Back side of the card */}
